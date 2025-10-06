1a952137c222e256b0b1dd888fd143e8751a62f1
Seydou Abdoul Nasser <seydounasser22@gmail.com>
2025-10-06 15:13:57 +0000

    feat: ajout du scanner QR et de la page de détails de l'œuvre, mise à jour des traductions et du style

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,6 +1,9 @@
 export default function Footer() {
   return (
-    <footer className="bg-gray-800 text-white p-4 text-center fixed bottom-0 w-full">
+    <footer
+      className="bg-gray-800 text-white p-4 text-center fixed bottom-0 w-full"
+      style={{ height: "3.5rem" }}
+    >
       <p>&copy; 2024 Musée 2.0. Tous droits réservés.</p>
     </footer>
   );
