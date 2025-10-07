3dd05829e57f2b477a2c050744af41d19b8c2c3b
Seydou Abdoul Nasser <seydounasser22@gmail.com>
2025-10-07 21:07:52 +0000

    Add placeholder SVG for unavailable images in assets

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,11 +1,11 @@
 import Footer from "./Footer";
-import Header from "./Header";
+import Header from "./header";
 
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
     <div className="flex flex-col min-h-screen">
       <Header />
-      <main className="flex-grow">{children}</main>
+      <main className="flex-grow bg-black">{children}</main>
       <Footer />
     </div>
   );
